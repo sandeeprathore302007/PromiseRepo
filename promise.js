540488f2dffb76aa0b95148f1a7c11d3fb806a6f
sandeeprathore302007 <sandeeprathore302007@gmail.com>
2023-09-22 10:09:10 +0530

    There is a new promise created

--- promise.js
+++ promise.js
@@ -1,2 +1,18 @@
 let promise = new Promise();
+let promise2 = new Promise(function(resolve,reject)
+{
+    setTimeout(function()
+    {
+        console.log("Hello World")
 
+    },2000)
+})
+promise2.then(function()
+{
+
+}).catch(function()
+{
+
+})
+
+// There is a new promise created
