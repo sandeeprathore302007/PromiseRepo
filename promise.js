95d001291ac980783e93e9a01486ff130023de2d
sandeeprathore302007 <sandeeprathore302007@gmail.com>
2023-09-22 13:53:36 +0530

    promise is removed

--- promise.js
+++ promise.js
@@ -1,18 +1,21 @@
-let promise = new Promise();
-let promise2 = new Promise(function(resolve,reject)
-{
-    setTimeout(function()
-    {
-        console.log("Hello World")
+// let promise = new Promise();
+// let promise2 = new Promise(function(resolve,reject)
+// {
+//     setTimeout(function()
+//     {
+//         console.log("Hello World")
 
-    },2000)
-})
-promise2.then(function()
-{
+//     },2000)
+// })
+// promise2.then(function()
+// {
 
-}).catch(function()
-{
+// }).catch(function()
+// {
 
-})
+// })
 
 // There is a new promise created
+let a = 5 ; 
+let c = String(a)
+console.log( typeof(c))
